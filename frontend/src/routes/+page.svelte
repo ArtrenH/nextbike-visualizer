ef1ed5b5f69c89639ad9b23329ab33ad402f25e3
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2026-01-18 00:40:14 +0100

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -92,7 +92,7 @@
 
     function formatTime(totalSeconds: number): string {
         const hours = Math.floor(totalSeconds / 3600);
-        const minutes = (totalSeconds % 3600) / 60;
+        const minutes = parseInt((totalSeconds % 3600) / 60);
         return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
     }
 </script>
